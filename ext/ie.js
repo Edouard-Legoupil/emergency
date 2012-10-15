b2d8c4c8fb0fee4af194a9b86c87954d4622c4f1
Edouard-Legoupil <edouard.legoupil@gmail.com>
2012-10-15 22:36:23 +0200

    commit

--- ext/ie.js
+++ ext/ie.js
@@ -1,8 +1,7 @@
-
 // Detect IE7 or IE8
-     var ua = $.browser;
+var ua = $.browser;
 if ( ua.msie && ua.version.slice(0,1) == "8" ) {
-alert('What? You are using "Internet Explorer 8"! this browser is too old and is not well supported. It is really time to change it!!!');
+alert('What? You are using "Internet Explorer 8"! this browser is too old and is not well supported. Maybe time has come to upgrade or change your browser?');
 } else if ( ua.msie && ua.version.slice(0,1) == "7" ) {
-alert('What? You are using "Internet Explorer 7"! this browser is too old and is not well supported. It is really time to change it!!!');
+alert('What? You are using "Internet Explorer 7"! this browser is too old and is not well supported. Maybe time has come to upgrade or change your browser?');
 }
